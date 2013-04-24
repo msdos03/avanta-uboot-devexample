7147ed31e5060a39023e23ac3f9e41d0f11cc725
Sean Howarth <showarth@google.com>
2013-04-24 14:55:51 -0700

    gflt200: enabled support for the pcf8523.
    Disabled support for Avanta internal RTC.
    
    Change-Id: I9d75a5652bf25b0e25ac01cfef1906d5d39ee0c7

--- include/configs/gflt200.h
+++ include/configs/gflt200.h
@@ -2,4 +2,9 @@
 /* configs/mv_kw2.h defines __CONFIG_H */
 #include <configs/mv_kw2.h>
 
+#undef MV_INCLUDE_RTC
+
+#define CONFIG_RTC_PCF8523
+#define CONFIG_SYS_I2C_RTC_ADDR	0x68
+
 #endif /* __CONFIG_H */
