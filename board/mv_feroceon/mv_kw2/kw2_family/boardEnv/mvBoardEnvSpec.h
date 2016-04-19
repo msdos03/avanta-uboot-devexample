78d05675c18351bfe294d7d52072a45d4102eb69
sniderd <sniderd@google.com>
2016-04-19 09:01:27 -0700

    Properly inverted GLFT110 Output Enable bits.
    Updated GLT300 Ouput Enable bits to match the kernel settings.
    Change-Id: I5d22a7754006d3c74e27716a42551f396b5413cf

--- board/mv_feroceon/mv_kw2/kw2_family/boardEnv/mvBoardEnvSpec.h
+++ board/mv_feroceon/mv_kw2/kw2_family/boardEnv/mvBoardEnvSpec.h
@@ -537,6 +537,9 @@ MPP#	NAME			IN/OUT
 /***************************************************************************
 ** GFLT110
 ****************************************************************************/
+/*spreadheet on GPIO settings:
+    https://goto.google.com/gflt110-gpio-config
+*/
 #define GFLT110_MPP0_7		0x22222220
 #define GFLT110_MPP8_15		0x00000002
 #define GFLT110_MPP16_23		0x00400000
@@ -562,23 +565,29 @@ MPP#	NAME			IN/OUT
 29 XVR_SD (in)
 33 TX_Fault/TX_indication
 37 TX_PD
-
-
 */
-#define GFLT110_GPP_OUT_ENA_LOW	(BIT0 | BIT14 | BIT15 | BIT16 | BIT17 | BIT18 | BIT19 | BIT22 | BIT23 | BIT24 | BIT27| BIT28 | BIT30 | BIT31)
-#define GFLT110_GPP_OUT_ENA_MID	(BIT0 | BIT3 | BIT4)
+
+
+// default all ones-inputs, invert for outputs
+#define GFLT110_GPP_OUT_ENA_LOW	(~(BIT12 | BIT13 | BIT20))
+#define GFLT110_GPP_OUT_ENA_MID	(~(BIT5))
+
 
 // BIT12 turns the LED blue.
 // BIT13 turns the LED red.
 #define GFLT110_GPP_OUT_VAL_LOW	BIT13
 #define GFLT110_GPP_OUT_VAL_MID	0x0
 
-#define GFLT110_GPP_POL_LOW		(BIT23)
+#define GFLT110_GPP_POL_LOW		0x0
 #define GFLT110_GPP_POL_MID		0x0
 
 /***************************************************************************
 ** GFLT300
 ****************************************************************************/
+/*spreadheet on GPIO settings:
+    https://goto.google.com/gflt300-gpio-config
+*/
+
 #define GFLT300_MPP0_7          0x22222220
 #define GFLT300_MPP8_15         0x05000002
 #define GFLT300_MPP16_23        0x00000000
@@ -586,62 +595,14 @@ MPP#	NAME			IN/OUT
 #define GFLT300_MPP32_37        0x00000004
 
 
-/* GPPs
-                                                     MUX  GPIO
-                                                           DIR
- 0 NC                                                  0   -
- 1 SPI0_MOSI (out)                                     2
- 2 SPI0_SCK (out)                                      2
- 3 SPI0_CSn[0] (out)                                   2
- 4 SPI0_MISO (in)                                      2
- 5 I2C0_SDA (inout)                                    2
- 6 I2C0_SCK (inout)                                    2
- 7 UA0_TXD (out)                                       2
-
- 8 UA0_RXD (in)                                        2
- 9 PON_LED_BLUE (out)                                  0   out  1
-10 PON_LED_RED (out)                                   0   out  0
-11 NC                                                  0   -
-12 NC                                                  0   -
-13 BOARD_VER[0] (in)                                   0   in
-14 GE_LINK_LED (out)                                   5
-15 BOARD_VER[1](in)                                    0   in
-
-16                                                     0   -
-17 SW_RESET  (in)                                      0   in
-18 BOARD_VER[2] (in)                                   0   in
-19 NC                                                  0   -
-20 NC                                                  0   -
-21 POS_TX_DIS  (out)                                   0   out  0
-22 NC                                                  0   -
-23 NC                                                  0   -
-
-24 PTP_TRIG_GEN(out)                                   4
-25 PTP_EVENT_REQ(in)                                   4
-26 GE_DATA_LED (out)                                   5
-27 PTP_CLK_IN (in)                                     4
-28 SPI_WP_L(out)(in for safety)                        0   in
-29 PON_LOS(in)                                         0   in
-30 DOLOS_DETECT_L                                      0   in
-31 UART1_TX(out)                                       4
-
-32 UART1_RX(in)                                        4
-33 ?NC DYING_GASP                                      0
-34 PU                                                  0   -
-35 PD                                                  0   -
-36 PON_RX_PMON(in)                                     0   in
-37 PON_PWR_EN(out)                                     0   out  1
-
-*/
-
-
-#define GFLT300_GPP_OUT_ENA_LOW	(BIT13 | BIT15 | BIT17 | BIT18 | BIT28 | BIT29 | BIT30)
-#define GFLT300_GPP_OUT_ENA_MID	(BIT4)
+// default all ones-inputs, invert for outputs
+#define GFLT300_GPP_OUT_ENA_LOW	(~(BIT9 | BIT10 | BIT21))
+#define GFLT300_GPP_OUT_ENA_MID	(~(BIT5))
 
 // BIT09 turns the LED blue.
 // BIT10 turns the LED red.
-#define GFLT300_GPP_OUT_VAL_LOW	(BIT9)
-#define GFLT300_GPP_OUT_VAL_MID	0x0
+#define GFLT300_GPP_OUT_VAL_LOW	(BIT9 | BIT21)
+#define GFLT300_GPP_OUT_VAL_MID	(0)
 
 #define GFLT300_GPP_POL_LOW	0x0
 #define GFLT300_GPP_POL_MID	0x0
